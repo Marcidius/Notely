e65e0528b924136e6b6005fc86e50116412943ff
ryan.king <ryan.king@beckshybrids.com>
2015-09-30 09:34:56 -0400

    Focus the title field when notes-form is loaded.

--- app/directives/bd-focus-on.js
+++ app/directives/bd-focus-on.js
@@ -1,24 +1,22 @@
 /**
  * Created by ryan.king on 9/29/2015.
  */
-(function() {
-    angular.module('notely')
-        .directive('bdFocusOn', bdFocusOn);
+angular.module('notely')
+    .directive('bdFocusOn', bdFocusOn);
 
-    //bdFocusOn[$inject] = [];
-    function bdFocusOn() {
-        return {
-            link: link
-        };
+//bdFocusOn[$inject] = [];
+function bdFocusOn() {
+    return {
+        restrict: 'A',
+        link: link
+    };
 
-        function link(scope, element, attr) {
-            debugger;
-            scope.$on(attr.bdFocusOn, function(e) {
-                debugger;
-                elem[0].focus();
-            });
-        }
+    function link(scope, elem, attr) {
+        //scope.$on(attr.bdFocusOn, function (e) {
+        //debugger;
+        elem.ready(function () {
+            elem[0].focus();
+        });
+        //});
     }
-
-
-})();
+}
